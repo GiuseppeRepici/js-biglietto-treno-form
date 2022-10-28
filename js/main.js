9547795795bb313dd69b0bb506c4a2c2f321eab4
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-10-28 16:00:04 +0200

    if ed else non va

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
-const namecompleted = document.form11.name.value;
+const button1 = document.getElementById("bottonecarica");
+let prezzominorenne
+let prezzomaggiore60
+let prova
+let prezzoperdistanza
 
-console.log(namecompleted);
+
+
+
+button1.addEventListener("click", 
+
+    function(){
+        const nomeCompleted = document.getElementById("fname").value;
+        const distanceKm = document.getElementById("fkm").value;
+        const rangeAge = document.getElementById("etarange").value;
+        let y = 0.21;
+        let prezzoperdistanza = distanceKm * y;
+        console.log(nomeCompleted);
+        console.log(distanceKm);
+        console.log(rangeAge);
+
+        const comparinome = document.getElementById("nomepasseggero");
+        comparinome.innerHTML = nomeCompleted ;
+
+        
+
+        if (rangeAge.value=20) {
+        prezzominorenne = parseFloat(prezzoperdistanza - (prezzoperdistanza * 20 / 100));
+        const prova = document.getElementById("costofinale");
+        prova.innerHTML = prezzominorenne.toFixed(2) ;
+        } else if (rangeAge.value=40) {
+                prezzomaggiore60 = parseFloat(prezzoperdistanza - (prezzoperdistanza * 40 / 100));
+        
+                const prova = document.getElementById("costofinale");
+
+                prova.innerHTML = prezzomaggiore60.toFixed(2) ;
+                
+        } else 
+             prova = document.getElementById("costofinale");
+
+             prova.innerHTML = prezzoperdistanza.toFixed(2) ;
+    }
+
+)
+
+
+
