8c7917cdbe16d5b4233eaf97be7b6817327c3f31
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-10-28 16:56:14 +0200

    tipo di biglietto non va

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const button1 = document.getElementById("bottonecarica");
 let prezzominorenne
 let prezzomaggiore60
 let prova
+let prova1
 let prezzoperdistanza
 
 
@@ -28,17 +29,24 @@ button1.addEventListener("click",
         prezzominorenne = parseFloat(prezzoperdistanza - (prezzoperdistanza * 20 / 100));
         const prova = document.getElementById("costofinale");
         prova.innerHTML = prezzominorenne.toFixed(2) ;
+        
+        prova1 = document.getElementById("tipobiglietto").innerHTML = "Biglietto per minorenni";
+        
+
         } else if (rangeAge == 40) {
                 prezzomaggiore60 = parseFloat(prezzoperdistanza - (prezzoperdistanza * 40 / 100));
-        
+                
                 const prova = document.getElementById("costofinale");
-
+                
                 prova.innerHTML = prezzomaggiore60.toFixed(2) ;
+
+                prova1 = document.getElementById("tipobiglietto").innerHTML = "Biglietto per over 65";
                 
         } else 
              prova = document.getElementById("costofinale").innerHTML = prezzoperdistanza.toFixed(2);
 
-             
+             prova1 = document.getElementById("tipobiglietto").innerHTML = "Biglietto intero";
+            
     }
 
 )
