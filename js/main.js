1b9660bd25a86ab292d4753ff1b58a01f09c1d7f
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-10-31 00:18:30 +0100

    js ok

--- js/main.js
+++ js/main.js
@@ -5,17 +5,47 @@ let prova
 let prova1
 let prezzoperdistanza
 const button2 = document.getElementById("bottoneannulla");
+const numerocasuale = document.getElementById("numcasuale");
+var z = Math.floor(Math.random() * (3));
+
+
+
+
+function generateRandomString(iLen) {
+    var sRnd = '';
+    var sChrs = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    for (var i = 0; i < iLen; i++) {
+      var randomPoz = Math.floor(Math.random() * sChrs.length);
+      sRnd += sChrs.substring(randomPoz, randomPoz + 1);
+    }
+    return sRnd;
+  }
+
+  var stringa_random = generateRandomString(4);
+  
+
+
+
+
 
 button2.addEventListener("click",
     function(){
         document.getElementById('fname').value = '';
         document.getElementById('fkm').value = '';
         document.getElementById('etarange').value = '';
+        
+        document.getElementById("costofinale").innerHTML = '';
+        document.getElementById("tipobiglietto").innerHTML = '';
+        document.getElementById("nomepasseggero").innerHTML= '';
+        numerocasuale.innerHTML = '' ;
+        document.getElementById("stringarndm").innerHTML = '';
     }
 
 )
 
 button1.addEventListener("click", 
+
+
 
     function(){
         const nomeCompleted = document.getElementById("fname").value;
@@ -26,7 +56,8 @@ button1.addEventListener("click",
         console.log(nomeCompleted);
         console.log(distanceKm);
         console.log(rangeAge);
-
+        numerocasuale.innerHTML = z ;
+        document.getElementById("stringarndm").innerHTML = stringa_random;
         const comparinome = document.getElementById("nomepasseggero");
         comparinome.innerHTML = nomeCompleted ;
 
